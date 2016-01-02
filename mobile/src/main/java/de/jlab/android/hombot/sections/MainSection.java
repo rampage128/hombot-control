143ae75bc6a6ec889f1e405e65f6e4b57ac90b9e
rampage128 <frederik.wolter@googlemail.com>
2016-01-02 21:32:48 +0100

    Add joystick control to main section

--- mobile/src/main/java/de/jlab/android/hombot/sections/MainSection.java
+++ mobile/src/main/java/de/jlab/android/hombot/sections/MainSection.java
@@ -1,14 +1,22 @@
 package de.jlab.android.hombot.sections;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import de.jlab.android.hombot.R;
 import de.jlab.android.hombot.SectionFragment;
 import de.jlab.android.hombot.common.core.HombotStatus;
+import de.jlab.android.hombot.common.settings.SharedSettings;
+import de.jlab.android.hombot.common.utils.JoyTouchListener;
 import de.jlab.android.hombot.core.HttpRequestEngine;
 import de.jlab.android.hombot.sections.common.StatusFragment;
 import de.jlab.android.hombot.utils.Colorizer;
@@ -26,6 +34,9 @@ public class MainSection extends SectionFragment {
         Button commandMode;
         Button commandHome;
         Button commandRepeat;
+
+        View joy;
+        TextView joyLabel;
     }
     private ViewHolder mViewHolder;
 
@@ -55,13 +66,18 @@ public class MainSection extends SectionFragment {
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getActivity());
+
         mViewHolder.statusDisplay = (StatusFragment) getChildFragmentManager().findFragmentById(R.id.ct_status);
         mViewHolder.commandStartStop = (Button) view.findViewById(R.id.cm_startstop);
         mViewHolder.commandTurbo = (Button) view.findViewById(R.id.cm_turbo);
         mViewHolder.commandMode = (Button) view.findViewById(R.id.cm_mode);
         mViewHolder.commandHome = (Button) view.findViewById(R.id.cm_home);
         mViewHolder.commandRepeat = (Button) view.findViewById(R.id.cm_repeat);
+        mViewHolder.joy = view.findViewById(R.id.ct_joy);
+        mViewHolder.joyLabel = (TextView)view.findViewById(R.id.ct_joy_label);
 
+        // INIT COMMANDS
         mViewHolder.commandTurbo.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 sendCommand(HttpRequestEngine.Command.TURBO);
@@ -104,6 +120,86 @@ public class MainSection extends SectionFragment {
             }
         });
 
+        // INIT JOYSTICK
+        int intervalDrive = Integer.parseInt(sp.getString(SharedSettings.PREF_JOY_INTERVAL_DRIVE, "800"));
+        int intervalTurn = Integer.parseInt(sp.getString(SharedSettings.PREF_JOY_INTERVAL_TURN, "800"));
+
+        mViewHolder.joy.setOnTouchListener(new JoyTouchListener(intervalDrive, intervalTurn, new JoyTouchListener.PushListener[]{
+                new JoyTouchListener.PushListener() {
+                    @Override
+                    public void onPush() {
+                        sendCommand(HttpRequestEngine.Command.JOY_FORWARD);
+                        Log.d("MOT", "F");
+                    }
+
+                    @Override
+                    public void onRelease() {
+                        sendCommand(HttpRequestEngine.Command.JOY_RELEASE);
+                        Log.d("MOT", "-");
+                    }
+                },
+                new JoyTouchListener.PushListener() {
+                    @Override
+                    public void onPush() {
+                        sendCommand(HttpRequestEngine.Command.JOY_RIGHT);
+                        Log.d("MOT", "R");
+                    }
+
+                    @Override
+                    public void onRelease() {
+                        sendCommand(HttpRequestEngine.Command.JOY_RELEASE);
+                        Log.d("MOT", "-");
+                    }
+                },
+                new JoyTouchListener.PushListener() {
+                    @Override
+                    public void onPush() {
+                        sendCommand(HttpRequestEngine.Command.JOY_BACK);
+                        Log.d("MOT", "B");
+                    }
+
+                    @Override
+                    public void onRelease() {
+                        sendCommand(HttpRequestEngine.Command.JOY_RELEASE);
+                        Log.d("MOT", "-");
+                    }
+                },
+                new JoyTouchListener.PushListener() {
+                    @Override
+                    public void onPush() {
+                        sendCommand(HttpRequestEngine.Command.JOY_LEFT);
+                        Log.d("MOT", "L");
+                    }
+
+                    @Override
+                    public void onRelease() {
+                        sendCommand(HttpRequestEngine.Command.JOY_RELEASE);
+                        Log.d("MOT", "-");
+                    }
+                },
+                new JoyTouchListener.PushListener() {
+                    @Override
+                    public void onPush() {
+                        sendCommand(HttpRequestEngine.Command.PAUSE);
+                        Log.d("MOT", "P");
+                    }
+
+                    @Override
+                    public void onRelease() { /* NO RELEASE FOR CENTER COMMAND */ }
+                }
+        }));
+
+        // MAKE JOYPAD "SQUARE" ACCORDING TO THE SMALLER AVAILABLE DIMENSION FIXME BUGGY ON ORIENTATION CHANGE
+        if (mViewHolder.joy.getViewTreeObserver().isAlive()) {
+            mViewHolder.joy.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+                @Override
+                public void onGlobalLayout() {
+                    int finalSize = Math.min(mViewHolder.joy.getMeasuredWidth(), mViewHolder.joy.getMeasuredHeight());
+                    mViewHolder.joy.setLayoutParams(new RelativeLayout.LayoutParams(finalSize, finalSize));
+                    getView().invalidate();
+                }
+            });
+        }
 
         Colorizer colorizer = getColorizer();
 
